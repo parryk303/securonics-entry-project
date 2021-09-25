38cd30732a7534b86663e79b42b8f39c568d44ed
Kyle Parry <kyle.parry.303@gmail.com>
2021-09-25 11:28:27 -0600

    Add dbconnect into api

--- pages/api/raForms/index.js
+++ pages/api/raForms/index.js
@@ -1,5 +1,19 @@
-import dbconnect from '../../../utils/dbconnect';
 import RaForm from '../../../models/RaForm';
+import mongoose from 'mongoose';
+
+const connection = {};
+
+const dbconnect = async () => {
+    if (connection.isConnected) {
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    connection.isConnected = db.connections[0].readyState;
+    console.log(connection.isConnected)
+  }
 
 dbconnect();
 
