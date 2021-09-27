8c94310548bcd3c8f239fa1cb6264788816240b9
Kyle Parry <kyle.parry.303@gmail.com>
2021-09-27 12:10:36 -0600

    Remove auth from _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
       <Layout><Component {...pageProps} /></Layout>
-    </Provider>
   )
 }
 
