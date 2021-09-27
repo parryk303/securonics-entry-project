e9033c37e51ba81a87e239c07bc9354d8101a806
Kyle Parry <kyle.parry.303@gmail.com>
2021-09-27 13:11:50 -0600

    Fix index 500

--- pages/index.js
+++ pages/index.js
@@ -8,31 +8,36 @@ const Index = ({ raForms }) => {
   return (
     <div className="raForms-container">
       <h1>Risk Assessment Forms</h1>
-      <div className="grid wrapper">
-        {raForms.map(raForm => {
-          return (
-            <div key={raForm._id}>
-              <Card>
-                <Card.Content>
-                  <Card.Header>
-                    <Link href={`/${raForm._id}`}>
-                      <a>{raForm.title}</a>
-                    </Link>
-                  </Card.Header>
-                </Card.Content>
-                <Card.Content extra>
-                  <Link href={`/${raForm._id}`} passHref>
-                    <Button primary>View</Button>
-                  </Link>
-                  <Link href={`/${raForm._id}/edit`} passHref>
-                    <Button primary>Edit</Button>
-                  </Link>
-                </Card.Content>
-              </Card>
-            </div>
-          )
-        })}
-      </div>
+      {raForms ?
+        (<div className="grid wrapper">
+                {raForms.map(raForm => {
+                  return (
+                    <div key={raForm._id}>
+                      <Card>
+                        <Card.Content>
+                          <Card.Header>
+                            <Link href={`/${raForm._id}`}>
+                              <a>{raForm.title}</a>
+                            </Link>
+                          </Card.Header>
+                        </Card.Content>
+                        <Card.Content extra>
+                          <Link href={`/${raForm._id}`} passHref>
+                            <Button primary>View</Button>
+                          </Link>
+                          <Link href={`/${raForm._id}/edit`} passHref>
+                            <Button primary>Edit</Button>
+                          </Link>
+                        </Card.Content>
+                      </Card>
+                    </div>
+                  )
+                })}
+              </div>
+        ) : (
+          <h1>Welcome!</h1>
+        )
+      }
     </div>
   )
 }
